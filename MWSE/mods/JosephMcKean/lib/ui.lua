fd3a8248c76aacb44b8fe98ca2c34c1c095b8a1f
Amalie <amaliegay@outlook.com>
2023-11-20 14:16:50 +0800

    ui.lua: ui.keyDownUIToggle

--- MWSE/mods/JosephMcKean/lib/ui.lua
+++ MWSE/mods/JosephMcKean/lib/ui.lua
@@ -1,8 +1,12 @@
----@param e keyDownEventData
+local ui = {}
 
-local function keyDownUIToggle(e, createMenu)
-	if not tes3.isKeyEqual({ actual = e, expected = config.key }) then return end
-	local menu = tes3ui.findMenu(uiids.menu)
+---@param actualKey JosephMcKean.lib.keybind
+---@param expectedKey JoseohMcKean.lib.keybind
+---@param uiid integer
+---@param createMenu function
+function ui.keyDownUIToggle(actualKey, expectedKey, uiid, createMenu)
+	if not tes3.isKeyEqual({ actual = actualKey, expected = expectedKey }) then return end
+	local menu = tes3ui.findMenu(uiid)
 	if menu then
 		menu.visible = not menu.visible
 	else
