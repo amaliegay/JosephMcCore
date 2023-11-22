157dce7a5418d35cbf42638c5cf1e01c42e435c5
Amalie <amaliegay@outlook.com>
2023-11-22 14:34:08 +0800

    Update tic-tac-toe.lua: gameBoard

--- MWSE/mods/JosephMcKean/mobilePhone/apps/tic-tac-toe.lua
+++ MWSE/mods/JosephMcKean/mobilePhone/apps/tic-tac-toe.lua
@@ -6,7 +6,7 @@ local log = require("JosephMcKean.lib.logging").createLogger(config, "tictactoe"
 local uiids = {
 	appIcon = tes3ui.registerID("MenuMobilePhone_TicTacToe_icon"),
 	mainRect = tes3ui.registerID("MenuMobilePhone_TicTacToe_mainRect"),
-	mainBlock = tes3ui.registerID("MenuMobilePhone_TicTacToe_mainBlock"),
+	gameBoard = tes3ui.registerID("MenuMobilePhone_TicTacToe_gameBoard"),
 }
 
 tictactoe.icon = { uiid = uiids.appIcon, path = "Textures\\jsmk\\mb\\ttt\\icon.dds" }
@@ -17,11 +17,9 @@ local function createButton() end
 local function createMenu(menu)
 	local mainRect = menu:createRect({ id = uiids.mainRect })
 	mainRect.width, mainRect.height = config.display.width, config.display.height
-	local mainBlock = mainRect:createBlock({ id = uiids.mainBlock })
-	mainBlock.absolutePosAlignY = 1
-	mainBlock.width = mainRect.width
-	mainBlock.autoHeight = true
-	mainBlock.flowDirection = tes3.flowDirection.topToBottom
+	local title = mainRect:createLabel({ id = uiids.title, text = "Tic Tac Toe" })
+	local statusDisplay = mainRect:createLabel({ id = uiids.statusDisplay, text = "It's X's turn." })
+	local gameBoard = mainRect:createBlock({ id = uiids.gameBoard })
 end
 
 tictactoe.uiids = uiids
