1ef618ada4b99c8eee531cc7bd4db563d72d2e91
Amalie <amaliegay@outlook.com>
2023-12-22 16:27:02 +0800

    Update init.lua: local Mod = require("JosephMcKean.lib.Mod")

--- MWSE/mods/JosephMcKean/lib/init.lua
+++ MWSE/mods/JosephMcKean/lib/init.lua
@@ -1,14 +1,5 @@
 local logging = require("JosephMcKean.lib.logging")
-
-local Mod = {}
-
----@return JosephMcCore.mod
-function Mod:new()
-	local mod = {} ---@type JosephMcCore.mod
-	setmetatable(mod, self)
-	self.__index = self
-	return mod
-end
+local Mod = require("JosephMcKean.lib.Mod")
 
 local lib = { Mod = Mod, logging = logging }
 return lib
