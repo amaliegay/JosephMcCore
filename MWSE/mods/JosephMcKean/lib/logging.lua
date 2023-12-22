9277048f44539728b526541015b1d4c0880dd97a
Amalie <amaliegay@outlook.com>
2023-12-22 15:55:17 +0800

    Update logging.lua:log(serviceName)

--- MWSE/mods/JosephMcKean/lib/logging.lua
+++ MWSE/mods/JosephMcKean/lib/logging.lua
@@ -2,11 +2,21 @@ local logging = require("logging.logger")
 
 local loggers = {} ---@type table<string,mwseLogger[]>
 
-local function createLogger(config, serviceName)
-	local logger = logging.new({ name = string.format("%s - %s", config.mod.name, serviceName), logLevel = config.logLevel })
-	if not loggers[config.mod.name] then loggers[config.mod.name] = {} end
-	table.insert(loggers[config.mod.name], logger)
+---@param mod JosephMcCore.mod
+---@param serviceName string?
+local function createLogger(mod, serviceName)
+	local config = mod.config
+	local name = config.mod.name
+	if serviceName then serviceName = name .. " - " .. serviceName end
+	local logger = logging.new({ name = serviceName, logLevel = config.logLevel })
+	if not loggers[name] then loggers[name] = {} end
+	table.insert(loggers[name], logger)
 	return logger
 end
 
-return { createLogger = createLogger }
+---@param serviceName string?
+local function log(serviceName)
+	return createLogger(mod, serviceName)
+end
+
+return { log = log }
