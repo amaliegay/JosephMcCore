6526df5dda6e8d595effb171d62b47184215ec6b
Amalie <amaliegay@outlook.com>
2023-12-25 14:33:28 +0800

    lib.ui

--- MWSE/mods/JosephMcKean/lib/init.lua
+++ MWSE/mods/JosephMcKean/lib/init.lua
@@ -1,5 +1,6 @@
 local Mod = require("JosephMcKean.lib.Mod")
+local Mod = require("JosephMcKean.lib.ui")
 local utils = require("JosephMcKean.lib.utils")
 
-local lib = { Mod = Mod, utils = utils }
+local lib = { Mod = Mod, ui = ui, utils = utils }
 return lib
